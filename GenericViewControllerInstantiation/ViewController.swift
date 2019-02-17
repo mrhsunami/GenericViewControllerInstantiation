070bc89f63298fbb31d9e54e71b2ef71aeab1a74
Nathan <nathan.hsu@icloud.com>
2019-02-16 19:36:02 -0800

    Initial Commit

---
+++ GenericViewControllerInstantiation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericViewControllerInstantiation
+//
+//  Created by Nathan Hsu on 2019-02-16.
+//  Copyright © 2019 Nathan Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
